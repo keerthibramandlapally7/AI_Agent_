c12d98e57f40b89ac0a0f2928d42c9082dc5a08f
Keerthi <keerthibramandlapally31@gmail.com>
2026-07-26 22:22:09 +0530

    update agent code

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ from pydantic import BaseModel
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import PydanticOutputParser
+from langchain.agents import create_agent
+
 
 load_dotenv()
-
+    
 class ResearchResponse(BaseModel):
     topic:str
     summary:str
@@ -15,3 +17,26 @@ class ResearchResponse(BaseModel):
 
 llm = ChatGoogleGenerativeAI(model="gemini-flash-latest")
 parser=PydanticOutputParser(pydantic_object=ResearchResponse)
+
+prompt = ChatPromptTemplate.from_messages([
+    ("system", 
+      """
+            You are a research assistant that will help generate a research paper.
+            Answer the user query and use neccessary tools. 
+            Wrap the output in this format and provide no other text\n{format_instructions}
+            """,
+    ),
+    ("placeholder", "{chat_history}"),
+    ("human", "{query}"),
+    ("placeholder", "{agent_scratchpad}"),
+]).partial(format_instructions=parser.get_format_instructions())
+
+agent=create_agent(
+    llm=llm,
+    prompt=prompt,
+    tools=[]
+)
+raw_response=agent.invoke({"query":"what is the impact of globalization?"})
+print(raw_response)
+structured_response=parser.parse(raw_response.get('output'))[0]['text']
+print(structured_response)
